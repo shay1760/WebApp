56d57c4536c38a5a4073ba4346526e8d48057b98
shay1760 <43196273+shay1760@users.noreply.github.com>
2024-10-05 00:59:37 +0530

    adding intial files

---
+++ src/db/index.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB=async ()=>{
+    try{
+        const connectionInstance=await MIDIConnectionEvent.connect("")
+    }
+}
